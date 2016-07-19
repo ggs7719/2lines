a5f83892dbc38a715dac73e38bb39efcba2fe926
Eric Chiu <c3366301@gmail.com>
2016-07-19 17:10:35 +0800

    ios api CRU test ok

--- app/controllers/api_v1/prenatals_controller.rb
+++ app/controllers/api_v1/prenatals_controller.rb
@@ -1,5 +1,30 @@
 class ApiV1::PrenatalsController < ApplicationController
+
   def index
     @prenatals = Prenatal.all
+  end
+
+  def create
+    @prenatal = Prenatal.new( prenatal_params )
+
+    if @prenatal.save
+      render :json => { :message => "Successfully created"}
+    else
+      render :json => { :message => "Validate failed" }, :status => 400
+    end
+  end
+
+  def create
+    @prenatal = Prenatal.update( prenatal_params )
+
+    if @prenatal.save
+      render :json => { :message => "Successfully updated"}
+    else
+      render :json => { :message => "Validate failed" }, :status => 400
+    end
+  end
+
+  def prenatal_params
+    params.require(:prenatal).permit(:title, :content, :message, :status, :count)
   end
 end
