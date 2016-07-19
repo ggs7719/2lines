4f9acbecae19731d36e287b53b58c045f0d34bba
Eric Chiu <c3366301@gmail.com>
2016-07-19 19:16:00 +0800

    close api controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,13 +1,12 @@
 class ApiController < ActionController::Base
-  # before_action :authenticate_user_from_token!
 
-  # def authenticate_user_from_token!
+  def authenticate_user_from_token!
 
-  #   if params[:auth_token].present?
-  #     user = User.find_by_ios_token( params[:auth_token] )
+    if params[:auth_token].present?
+      user = User.find_by_ios_token( params[:auth_token] )
 
-  #     sign_in(user, store: false) if user
-  #   end
-  # end
+      sign_in(user, store: false) if user
+    end
+  end
 
 end
