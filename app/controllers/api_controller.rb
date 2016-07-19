3a2bec309b812ce39e15e8af7fb066b0c153f881
Eric Chiu <c3366301@gmail.com>
2016-07-19 19:24:11 +0800

    delete api controller method

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,12 +1,5 @@
 class ApiController < ActionController::Base
 
-  def authenticate_user_from_token!
 
-    if params[:auth_token].present?
-      user = User.find_by_ios_token( params[:auth_token] )
-
-      sign_in(user, store: false) if user
-    end
-  end
 
 end
