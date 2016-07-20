560daf69e71e933cd36e53ed18af82513c4077e6
ggs7719 <ggs7719@email.com>
2016-07-20 18:56:09 +0800

    產檢資料表CRUD

--- app/controllers/prenatals_controller.rb
+++ app/controllers/prenatals_controller.rb
@@ -10,6 +10,7 @@ class PrenatalsController < ApplicationController
   # GET /prenatals/1
   # GET /prenatals/1.json
   def show
+    @prenatal = Prenatal.find(params[:id])
   end
 
   # GET /prenatals/new
@@ -19,6 +20,7 @@ class PrenatalsController < ApplicationController
 
   # GET /prenatals/1/edit
   def edit
+    @prenatal = Prenatal.find(params[:id])
   end
 
   # POST /prenatals
@@ -28,7 +30,7 @@ class PrenatalsController < ApplicationController
 
     respond_to do |format|
       if @prenatal.save
-        format.html { redirect_to @prenatal, notice: 'Prenatal was successfully created.' }
+        format.html { redirect_to @prenatal, notice: '已新增產檢記錄！' }
         format.json { render :show, status: :created, location: @prenatal }
       else
         format.html { render :new }
@@ -40,25 +42,27 @@ class PrenatalsController < ApplicationController
   # PATCH/PUT /prenatals/1
   # PATCH/PUT /prenatals/1.json
   def update
-    respond_to do |format|
+    # respond_to do |format|
+    @eprenatal = Prenatal.find(params[:id])
       if @prenatal.update(prenatal_params)
-        format.html { redirect_to @prenatal, notice: 'Prenatal was successfully updated.' }
-        format.json { render :show, status: :ok, location: @prenatal }
-      else
-        format.html { render :edit }
-        format.json { render json: @prenatal.errors, status: :unprocessable_entity }
-      end
+      #   format.html { redirect_to prenatals_path, notice: '已更新產檢記錄！' }
+      #   format.json { render :show, status: :ok, location: @prenatal }
+      # else
+      #   format.html { render :edit }
+      #   format.json { render json: @prenatal.errors, status: :unprocessable_entity }
+       end
+       redirect_to prenatals_path
     end
-  end
 
   # DELETE /prenatals/1
   # DELETE /prenatals/1.json
   def destroy
     @prenatal.destroy
-    respond_to do |format|
-      format.html { redirect_to prenatals_url, notice: 'Prenatal was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to prenatals_path
+    # respond_to do |format|
+    #   format.html { redirect_to prenatals_url, notice: '已刪除產檢記錄！' }
+    #   format.json { head :no_content }
+    #end
   end
 
   private
@@ -67,8 +71,12 @@ class PrenatalsController < ApplicationController
       @prenatal = Prenatal.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def prenatal_params
-      params.fetch(:prenatal, {})
+      params.require(:prenatal).permit(:title, :content, :date, :status)
     end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    # def prenatal_params
+    #   params.fetch(:prenatal, {})
+    # end
 end
