a1ea4618d0d34fa687a7ff8a722efdf0e5c57788
Eric Chiu <c3366301@gmail.com>
2016-07-26 20:05:05 +0800

    fix cant send mail bug on server

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default :from => "jimmyhoo7719@gmail.com"
+  default :from => "twolines <jimmyhoo7719@gmail.com>"
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
